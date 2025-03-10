3bad2b05fac266c472c7025e69e178db6f9cc253
SUTHAKAR SABABADI <117422056+SuthakarSababadi@users.noreply.github.com>
2025-03-10 23:31:26 +0800

    blade file

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -1,3 +1,7 @@
-<div>
-   {{$this->table}}
-</div>
+<x-filament::section class="max-w-7xl mx-auto items-center justify-center">
+    <x-slot name="heading">
+    Tickets List
+ </x-slot>
+
+ {{$this ->table}}
+</x-filament::section>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use App\Models\User;
 use App\Livewire\ListTickets;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
 
-  // dd( User::find(1)->hasPermission('category_create'));
+    // dd( User::find(1)->hasPermission('category_create'));
     return view('welcome');
 });
 
-Route::get('tickets',ListTickets::class)->name('tickets.index');
+Route::get('tickets', ListTickets::class)->name('tickets.index');
